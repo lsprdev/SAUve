20219dcfd02a3e2b162a06211e369db5edc987ce
Gabriel L. Pereira <ven@Hackintosh.local>
2023-04-11 09:22:21 -0300

    ClassesTile added

---
+++ lib/views/widgets/classes_tile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ClassesTile extends StatelessWidget {
+  const ClassesTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
